b7f6dbdaea04eb5365058bdb1d63e425972c5879
Killian Halpin <you@example.com>
2022-09-17 14:15:01 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -7,13 +7,29 @@ const aCar = {
     },
     registration: { year: 201, county: "WD", number: 1058 }
   };
-  
-  console.log(aCar.owner + " drives a " + aCar.type.make);
-  console.log(
-    "Reg. = " +
-      aCar.registration.year +
-      "-" +
-      aCar.registration.county +
-      "-" +
-      aCar.registration.number
-  );
+ 
+ console.log(aCar.owner + " drives a " + aCar.type.make);
+ console.log(
+   "Reg. = " +
+     aCar.registration.year +
+     "-" +
+     aCar.registration.county +
+     "-" +
+     aCar.registration.number
+ );
+ 
+ aCar.milage = 10000
+ aCar.color = {
+   exterior: "red",
+   interior: { texture: "leather", shade: "cream" }
+ }
+ 
+ console.log(
+   "It is a " +
+     aCar.color.exterior +
+     " car, " +
+     aCar.milage +
+     " milage, with " +
+     aCar.color.interior.texture +
+     " interior."
+ );
