2fe98c3e70146b46eee009e13a189f97b6def288
Killian Halpin <you@example.com>
2022-09-17 14:20:15 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,9 @@
 const aCar = {
     owner: "Joe Bloggs",
+    previous_owners: [
+        { name: "Pat Smith", address: "1 Main Street" },
+        { name: "Sheila Dwyer", address: "2 High Street" }
+    ],
     type: {
       make: "Toyota",
       model: "Corolla",
@@ -33,3 +37,10 @@ const aCar = {
      aCar.color.interior.texture +
      " interior."
  );
+ console.log(
+    "First owner: " +
+      aCar.previous_owners[0].name +
+      ' - ' +
+      aCar.previous_owners[0].address
+  )
+  
